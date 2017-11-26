d8f94ab427ac2003a20301e6d71dbb5f0dacee8d
Bijiabo <bijiabo@gmail.com>
2017-11-26 15:05:57 +0800

    Add: * 添加对 Github API token 的支持 * 添加 Lambda 部署配置

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import re
 import base64
 import mistune
+from os import environ
 from werkzeug.routing import BaseConverter
 class RegexConverter(BaseConverter):
     def __init__(self, map, *args):
@@ -12,23 +13,29 @@ class RegexConverter(BaseConverter):
 app = Flask(__name__)
 app.url_map.converters['regex'] = RegexConverter
 
-owner = 'bijiabo'
-repo = 'blog.te.dog'
-branch = 'master'
+owner = environ.get('owner', 'owner')
+repo = environ.get('repo', 'blog.te.dog')
+branch = environ.get('branch', 'master')
+token = environ.get('token', '')
+headers = {'Authorization': 'token {token}'.format(token=token)}
 
-# @app.route('/')
-# @app.route('/<path>')
 @app.route('/<regex(".*"):path>')
 def index(path=''):
     request_url = 'https://api.github.com/repos/{owner}/{repo}/contents/{path}?ref={branch}'.format(owner=owner, repo=repo, branch=branch, path=path)
     print(request_url)
-    request_result = requests.get(request_url)
+    request_result = requests.get(request_url, headers=headers)
     print(request_result.json())
     content = request_result.json()
     content_is_list = type(content) == list
     if not content_is_list:
         content_markdown_string = base64.b64decode(content['content']).decode('utf-8')
         content['content'] = mistune.markdown(content_markdown_string)
+        # get lastest commit information
+        request_url = 'https://api.github.com/repos/{owner}/{repo}/commits?path={path}&ref={branch}'.format(owner=owner, repo=repo, branch=branch, path=path)
+        print(request_url)
+        request_result = requests.get(request_url, headers=headers)
+        print(request_result.json())
+        content['commit_info'] = request_result.json()[0]
     return render_template('index.html', content=content, re=re, type=type, list=list)
 
 @app.route('/demo')
